8c7505ea506359464e3c31042f2febec67552100
Jhburns <jburns@chapman.edu>
2019-10-29 10:56:36 -0700

    Adding scene management buttons

---
+++ ClientDevelopmentGame/Assets/Scripts/Scenery.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Scenery : MonoBehaviour {
+    public string nextSceneName;
+
+    public void reload()
+    {
+        Scene current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.name);
+    }
+
+    public void done()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
