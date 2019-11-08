58012cd993149dbf48a314e5d7363e9ba0e2430d
Jhburns <jburns@chapman.edu>
2019-11-08 11:17:46 -0800

    Outlining Exporter Code

--- Server/Exporter/Exporter.cs
+++ Server/Exporter/Exporter.cs
@@ -1,16 +1,32 @@
 namespace Export.App.Main
 {
     using System;
+    using System.Collections.Generic;
+
+    using MongoDB.Bson;
     using MongoDB.Driver;
 
     public class Export
     {
+        private static IMongoDatabase db;
+
         public static void Main(string[] args)
         {
             MongoClient client = new MongoClient(@"mongodb://db:27017");
-            IMongoDatabase db = client.GetDatabase("ec");
+            db = client.GetDatabase("ec");
 
             Console.WriteLine("Hello ");
+        }
+
+        public static void PromptOption()
+        {
+        }
+
+        // public static List<BsonValue> SearchSession(string id)
+        // {
+        // }
+        public static void OutputToFile(string content, string id)
+        {
         }
     }
 }
