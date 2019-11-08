322ed254a58308b31ecaaf9e688a01e90a64c2d2
Jhburns <jburns@chapman.edu>
2019-11-08 12:36:13 -0800

    Found data is exported

--- Server/Exporter/Exporter.cs
+++ Server/Exporter/Exporter.cs
@@ -2,6 +2,7 @@ namespace Export.App.Main
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     using MongoDB.Bson;
     using MongoDB.Driver;
@@ -16,16 +17,46 @@ namespace Export.App.Main
             db = client.GetDatabase("ec");
 
             Console.WriteLine("Hello ");
+
+            SearchSession("YXN7");
         }
 
-        public static void PromptOption()
+        private static void PromptOption()
         {
         }
 
-        // public static List<BsonValue> SearchSession(string id)
-        // {
-        // }
-        public static void OutputToFile(string content, string id)
+        private static async Task<List<BsonDocument>> SearchSession(string id)
+        {
+            var sessionDocs = db.GetCollection<BsonDocument>($"sessions.{id}");
+            List<BsonDocument> docs = await sessionDocs
+                .Find(Builders<BsonDocument>.Filter.Empty)
+                .SortByDescending(d => d["info"]["realtimeSinceStartup"])
+                .ToListAsync();
+
+            return docs;
+        }
+
+        private static bool CheckSession(string sessionId)
+        {
+            var sessions = db.GetCollection<BsonDocument>("sessions");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("id", sessionId);
+            var sessionDoc = sessions.Find(filter).FirstOrDefault();
+
+            if (sessionDoc == null)
+            {
+                return false;
+            }
+
+            if (sessionDoc["isOpen"] == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void OutputToFile(string content, string id)
         {
         }
     }
