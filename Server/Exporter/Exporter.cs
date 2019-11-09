d13f29532914b5a8d86cbb29112e1127e580d371
Jhburns <jburns@chapman.edu>
2019-11-08 18:18:24 -0800

    CLeaned up Exporter

---
+++ Server/Exporter/Exporter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Export.App.Main
+{
+    class Export
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
