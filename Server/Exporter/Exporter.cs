523526426b6ed7821b0f5feb84fef7b93b9117d0
Jhburns <jburns@chapman.edu>
2019-11-08 18:55:51 -0800

    Finished Mongo integration

--- Server/Exporter/Exporter.cs
+++ Server/Exporter/Exporter.cs
@@ -1,11 +1,15 @@
 namespace Export.App.Main
 {
     using System;
+    using MongoDB.Driver;
 
     public class Export
     {
         public static void Main(string[] args)
         {
+            MongoClient client = new MongoClient(@"mongodb://db:27017");
+            IMongoDatabase db = client.GetDatabase("ec");
+
             Console.WriteLine("Hello ");
         }
     }
