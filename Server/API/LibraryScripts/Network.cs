64cd738008ab257a8a578b2112e122cecf28d09d
Jhburns <jburns@chapman.edu>
2019-12-12 23:46:57 -0800

    Removing SVD

--- Server/API/LibraryScripts/Network.cs
+++ Server/API/LibraryScripts/Network.cs
@@ -1,12 +1,3 @@
-/*
-- Name: Jonathan Hirokazu Burns
-- ID: 2288851
-- email: jburns@chapman.edu
-- Course: 353-01
-- Assignment: Submission #2
-- Purpose: To be a general purpose networking library
-*/
-
 namespace Network
 {
     using System.Collections;
@@ -19,38 +10,16 @@ namespace Network
     using Newtonsoft.Json;
     using Newtonsoft.Json.Bson;
 
-    /*
-     * Extensions
-     * See Nancy documentation
-     */
     public static class Extensions
     {
-        /*
-         * FromByteArray
-         * Params:
-         * - formatter: see Nancy documentation
-         * - body: the content of the responce
-         * - contentType: the HTTP content type flag
-         * Returns: a Response object, see Nancy documentation
-         */
         public static Response FromByteArray(this IResponseFormatter formatter, byte[] body, string contentType = null)
         {
             return new ByteArrayResponse(body, contentType);
         }
     }
 
-    /*
-     * ByteArrayResponse
-     * Custom response type
-     */
     public class ByteArrayResponse : Response
     {
-        /*
-         * FromByteArray
-         * Params:
-         * - body: the content of the responce
-         * - contentType: the HTTP content type flag
-         */
         public ByteArrayResponse(byte[] body, string contentType = null)
         {
             this.ContentType = contentType ?? "application/octet-stream";
