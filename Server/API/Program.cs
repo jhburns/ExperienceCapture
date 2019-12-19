67afcce63b8bf40f2b644516402ae46690ae3091
jhburns <jburns@chapman.edu>
2019-12-19 15:59:10 -0800

    Health endpoint replaced

--- Server/API/Program.cs
+++ Server/API/Program.cs
@@ -18,12 +18,20 @@
     }
 }
 */
-namespace Carter.App
+namespace Carter.App.Hosting
 {
-    public class API
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
     {
         public static void Main(string[] args)
         {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build();
+
+            host.Run();
         }
     }
 }
--- Server/API/Routes/Health.cs
+++ Server/API/Routes/Health.cs
@@ -11,3 +11,25 @@
     }
 }
 */
+
+namespace Carter.Route.Health
+{
+    using Carter;
+    using Microsoft.AspNetCore.Http;
+
+    public class HomeModule : CarterModule
+    {
+        public HomeModule()
+        {
+            this.Get("/", (req, res) =>
+            {
+                return res.WriteAsync("The api server is running.");
+            });
+
+            this.Get("/health", (req, res) =>
+            {
+                return res.WriteAsync("OK");
+            });
+        }
+    }
+}
