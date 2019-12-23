04667368aeaa839231043a309dc9febf0465ebbc
jhburns <jburns@chapman.edu>
2019-12-22 18:33:24 -0800

    Installing react router and query string

--- Server/WebUI/src/libs/userManagement.js
+++ Server/WebUI/src/libs/userManagement.js
@@ -3,10 +3,22 @@ import { gapi } from 'gapi-script';
 import { postData } from 'libs/fetchExtra';
 
 async function submitUser(isMock=false, user, onError) {
-	console.log("ID: " + user.getId());
-	console.log("secure token: " + user.getAuthResponse().id_token);
+	var userData = {
+		idToken: "This.is.not.a.real.id.token",
+		id: 4321234,
+		signUpToken: "waa"
+	};
+
+	if (!isMock) {
+		userData = {
+			idToken: user.getAuthResponse().id_token,
+			id: user.getId(),
+			signUpToken: "waa"
+		};
+	}
+
 	try {
-		const replyData = await postData('/api/v1/', {});
+		const replyData = await postData('/api/v1/users/', userData);
 		console.log(JSON.stringify(replyData));
 	} catch (error) {
 		console.error(error);
