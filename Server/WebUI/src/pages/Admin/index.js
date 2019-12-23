37d92569a2818395c773fa8262a74653c0bde677
jhburns <jburns@chapman.edu>
2019-12-23 15:41:08 -0800

    Admin sign in redircts succefully

--- Server/WebUI/src/pages/Admin/index.js
+++ Server/WebUI/src/pages/Admin/index.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 import queryString from 'query-string';
 
 import { postData } from 'libs/fetchExtra';
+import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 
 class Admin extends Component {
-  render() {
-    return (
-      <div>
-        <p>Redirecting...</p>
-        <p>Unless password is invalid</p>
-      </div>
-    );
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isError: false,
+      accessToken: null
+    }
+
+    this.successCallback = this.onSuccess.bind(this);
+	  this.errorCallback = this.onError.bind(this);
+  }
+
+  onSuccess(token) {
+    this.setState({
+      accessToken: token
+    });
+  }
+  
+  onError() {
+    this.setState({
+      isError: true
+    });
   }
 
   async componentDidMount() {
     const query = queryString.parse(this.props.location.search);
-    console.log(query.password);
 
     try {
       const data = {
@@ -25,12 +40,39 @@ class Admin extends Component {
       const reply = await postData("/api/v1/users/signUp/admin/", data);
 
       if (reply.ok) {
-        console.log(await reply.text());
+        this.successCallback(await reply.text());
       } else {
+        this.errorCallback();
         throw Error(reply.error);
       }
     } catch (error) {
+      this.errorCallback();
       console.error(error);
+    }
+  }
+
+  render() {
+    if (this.state.isError) {
+      return (
+        <div>
+          <p>Password is Invalid</p>
+          <p>Check console</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>Redirecting...</p>
+          <Router>
+            <Redirect
+              to={{
+                pathname: "/signUp",
+                search: queryString.stringify({signUpToken: this.state.accessToken}),
+              }}
+            />
+          </Router>
+        </div>
+      );
     }
   }
 }
