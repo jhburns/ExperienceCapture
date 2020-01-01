2007d0e1b73eeb030021049dba3625999fd71242
jhburns <jburns@chapman.edu>
2019-12-31 18:38:15 -0800

    Finished refactoring exporter prep

--- Server/Exporter/Exporter.cs
+++ Server/Exporter/Exporter.cs
@@ -1,51 +1,21 @@
-namespace Export.App.Main
+namespace Exporter.App.Main
 {
     using System;
-    using System.Collections.Generic;
-    using System.IO;
 
-    using MongoDB.Bson;
-    using MongoDB.Driver;
+    using Exporter.App.ExportHandler;
 
-    public class Export
+    public class Entry
     {
-        private static readonly IMongoDatabase DB = new MongoClient(@"mongodb://db:27017")
-            .GetDatabase("ec");
-
         public static void Main(string[] args)
         {
-        }
-
-        private static async void SortSession(string id)
-        {
-            var sessionCollection = DB.GetCollection<BsonDocument>($"sessions.{id}");
-
-            List<BsonDocument> docs = await sessionCollection
-                .Find(Builders<BsonDocument>.Filter.Empty)
-                .SortByDescending(d => d["info"]["realtimeSinceStartup"])
-                .ToListAsync();
-
-            string docsTotal = "[";
-            foreach (BsonDocument d in docs)
+            try
             {
-                d.Remove("_id");
-                docsTotal += d.ToJson() + ",";
+                ExportHandler.Start();
             }
-
-            docsTotal = docsTotal.Substring(0, docsTotal.Length - 1);
-            docsTotal += "]";
-
-            OutputToFile(docsTotal, id);
-        }
-
-        private static void OutputToFile(string content, string id)
-        {
-            string seperator = Path.DirectorySeparatorChar.ToString();
-            string path = $".{seperator}data{seperator}exported{seperator}{id}.sorted.json";
-
-            Console.WriteLine("Outputted to file: " + path);
-
-            System.IO.File.WriteAllText(path, content);
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
