ad6e2f2dce670dd78e114bdbb5233921c2eaa8bb
jhburns <jburns@chapman.edu>
2020-02-02 13:54:22 -0800

    Putting elements in containers

--- Server/WebUI/src/pages/NormalSignIn/index.js
+++ Server/WebUI/src/pages/NormalSignIn/index.js
@@ -3,31 +3,35 @@ import logo from 'logo.svg';
 
 import GoogleSignIn from "components/GoogleSignIn";
 
-import { Row, Col, } from '@bootstrap-styled/v4';
-import { Wrapper, } from 'pages/NormalSignIn/style';
+import { Container, Row, Col, } from '@bootstrap-styled/v4';
+import { Wrapper } from 'pages/NormalSignIn/style';
 
 class NormalSignInPage extends Component {
   render() {
     return (
-      <Wrapper>
-        <Row className="justify-content-center" noGutters={true}>
-          <Col xs={4} >
-            <img src={logo} alt="logo" />
-          </Col>
-        </Row>
-        <Row noGutters={true} className="mt-3">
-          <Col></Col>
-          <Col xs={10} >
-            <h1 className="text-center">
-              Experience <br /> Capture 
-            </h1>
-          </Col>
-          <Col></Col>
-        </Row>
-        <Row noGutters={true}>
-          <GoogleSignIn clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} />
-        </Row>
-      </Wrapper>
+      <Container>
+        <Wrapper>
+          <Row className="justify-content-center" noGutters={true}>
+            <Col xs={4} >
+              <img src={logo} alt="logo" />
+            </Col>
+          </Row>
+          <Row noGutters={true} className="mt-3 mb-5">
+            <Col></Col>
+            <Col xs={10} >
+              <h1 className="text-center">
+                Experience <br /> Capture 
+              </h1>
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row className="justify-content-center" noGutters={true}>
+            <Col xs={8} >
+              <GoogleSignIn clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} />
+            </Col>
+          </Row>
+        </Wrapper>
+      </Container>
     );
   }
 }
