a0704677e2372fc40bc5ce8ae9f5a6aca274074c
jhburns <jburns@chapman.edu>
2020-02-02 19:37:32 -0800

    Session page styled

--- Server/WebUI/src/components/SingleSession/index.js
+++ Server/WebUI/src/components/SingleSession/index.js
@@ -4,31 +4,42 @@ import moment from 'moment';
 
 import { Link } from 'react-router-dom';
 
-import { Row, } from '@bootstrap-styled/v4';
+import { Row, Col } from '@bootstrap-styled/v4';
+
+import { Wrapper } from 'components/SingleSession/style';
+
 
 class SingleSession extends Component {
   render() {
     const isExportDisabled = this.props.sessionData.isPending || this.props.sessionData.isExported;
 
     return (
-      <div>
-        <Row><p>Session: {this.props.sessionData.id}</p></Row>
-        <p>Captured By: {this.props.sessionData.fullname}</p>
-        <p>Date: {moment(this.props.sessionData.createdAt).format("MMM Do YY hh:mm a")}</p>
-        <button
-          onClick={this.props.onExport}
-          disabled={isExportDisabled}
-        >
-          Export
-        </button>
-        <Link 
-          to={`/api/v1/sessions/${this.props.sessionData.id}/export/`}
-          target="_blank" 
-          download
-        >
-          Download
+      <Wrapper>
+        <Row>
+          <Col>
+            <h2>Session: {this.props.sessionData.id}</h2>
+            <h5>Captured By: {this.props.sessionData.fullname}</h5>
+            <h5 className="mb-4">
+              {moment(this.props.sessionData.createdAt).format("MMM Do YY hh:mm a")}
+            </h5>
+            <button
+              onClick={this.props.onExport}
+              disabled={isExportDisabled}
+              className="btn btn-dark mr-2"
+            >
+              Export
+            </button>
+            <Link
+              to={`/api/v1/sessions/${this.props.sessionData.id}/export/`}
+              target="_blank"
+              download
+              className="btn btn-outline-dark"
+            >
+              Download
         </Link>
-      </div>
+          </Col>
+        </Row>
+      </Wrapper>
     )
   }
 }
