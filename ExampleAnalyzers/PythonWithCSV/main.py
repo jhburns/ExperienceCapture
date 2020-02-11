e059de43635f2108f0ba42de47506381d218c0b2
jhburns <jburns@chapman.edu>
2020-02-10 17:26:53 -0800

    Cleaning up the python csv code a bit

--- ExampleAnalyzers/PythonWithCSV/main.py
+++ ExampleAnalyzers/PythonWithCSV/main.py
@@ -9,8 +9,8 @@ for file in os.listdir("./data"):
         csv_filepaths.append(os.path.join("./data", file))
 
 responce_times = []
-rt = "gameObjects.UIController.responceTime"
-ir = "gameObjects.UIController.isResponding"
+rt_key = "gameObjects.UIController.responceTime"
+ir_key = "gameObjects.UIController.isResponding"
 
 # Looks for unique keys of responceTime key
 for filepath in csv_filepaths:
@@ -18,14 +18,14 @@ for filepath in csv_filepaths:
 		reader = csv.DictReader(csvfile)
 
 		for row in reader:
-			responce_time = Decimal(row[rt])
+			responce_time = Decimal(row[rt_key])
 
-			if responce_time not in responce_times and row[ir] == "False":
+			if responce_time not in responce_times and row[ir_key] == "False":
 				responce_times.append(responce_time)
 
 responce_times.remove(Decimal(0.0)) # cleaning up data, initial state is discarded 
 
-responce_times_str = list(map(str, responce_times)) # Change decimals to strings so they print correctly
+responce_times_str = list(map(float, responce_times)) # Change decimals to strings so they print correctly
 
 print("Every responce time (sec): {}".format(responce_times_str))
 print("Mean reaction time is: {}".format(float(sum(responce_times) / len(responce_times)))) 
