64a131ec2268246bfa4fa6641b5c5735e2ca2113
jhburns <jburns@chapman.edu>
2020-03-13 21:46:36 -0700

    Fixing footer margins

--- Server/WebUI/src/components/Footer/index.js
+++ Server/WebUI/src/components/Footer/index.js
@@ -8,7 +8,7 @@ class Footer extends Component {
     // Only component that gets top spacing besides Menu
     // Because adding consistent bottom spacing would be harder to maintain
     return (
-      <Wrapper className="mt-5">
+      <Wrapper className="mt-5 mb-5">
         <Row className="m-0">
           <Col className="text-center">
             <Item
