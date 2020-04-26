514520cdd8e22318d2f269d9f920977c93351327
jhburns <jburns@chapman.edu>
2020-04-25 18:28:18 -0700

    Exporter merged with API

---
+++ Server/API/Export/Exporter.cs
@@ -0,0 +1,546 @@
+namespace Carter.App.Export.Main
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Carter.App.Export.JsonHelper;
+
+    using CsvHelper;
+    using CsvHelper.Configuration;
+
+    using HandlebarsDotNet;
+
+    using Minio;
+    using Minio.Exceptions;
+
+    using MongoDB.Bson;
+    using MongoDB.Bson.IO;
+    using MongoDB.Driver;
+
+    public class ExportHandler
+    {
+        private static readonly IMongoDatabase DB = new MongoClient(@"mongodb://db:27017").GetDatabase("ec");
+        private static readonly MinioClient OS = new MinioClient("os:9000", "minio", "minio123");
+
+        private static readonly string Seperator = Path.DirectorySeparatorChar.ToString();
+
+        private static string sessionId;
+        private static string prefix;
+
+        private static int currentSceneIndex;
+        private static string currentSceneName;
+        private static List<string> currentHeader;
+
+        public static void Entry(object id)
+        {
+            _ = MainAsync((string)id);
+        }
+
+        private static async Task MainAsync(string id)
+        {
+            sessionId = id;
+
+            try
+            {
+                var jobId = Guid.NewGuid();
+                prefix = $"./{Seperator}exporter_temporary_files{Seperator}{jobId}{Seperator}";
+                string outFolder = $"{prefix}exported{Seperator}";
+                string zipFolder = $"{prefix}zipped{Seperator}";
+                string tempFolder = $"{prefix}temporary{Seperator}CSVs{Seperator}";
+
+                Directory.CreateDirectory(outFolder);
+                Directory.CreateDirectory($"{outFolder}CSVs{Seperator}");
+
+                Directory.CreateDirectory(zipFolder);
+                Directory.CreateDirectory(tempFolder);
+
+                ConfigureJsonWriter();
+                await ExportSession();
+
+                string outLocation = zipFolder + $"{sessionId}_session_exported.zip";
+                ZipFolder(outFolder, outLocation);
+
+                await Upload(outLocation);
+                await UpdateDoc();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            // Uncomment to make it so the program stays open, for debugging
+            // System.Threading.Thread.Sleep(100000000);
+        }
+
+        private static async Task ExportSession()
+        {
+            currentHeader = new List<string>();
+            currentSceneIndex = -1; // To play nice with the proccessing code
+
+            var workloads = await GetWorkloads();
+
+            var ws = new JsonWriterSettings()
+            {
+                Indent = false,
+                OutputMode = JsonOutputMode.Strict,
+            };
+
+            ToJsonStart("raw");
+            ToJsonStart("sessionInfo");
+            ToJsonStart("onlyCaptures");
+
+            for (int i = 0; i < workloads.Count; i++)
+            {
+                // Step should be any of the workloads, except the last
+                // First used because if it isn't a step then offset is zero anyway
+                int offset = (int)(workloads[0] * i);
+                List<BsonDocument> sessionSorted = await SortSession((int)workloads[i], offset);
+
+                bool isFirst = i == 0;
+                ToJson(sessionSorted, "raw", isFirst, ws);
+
+                var (otherCaptures, sceneBlocks) = ProcessScenes(sessionSorted);
+                if (otherCaptures.Count > 0)
+                {
+                    ToJson(otherCaptures, "sessionInfo", isFirst);
+                }
+
+                foreach (var block in sceneBlocks)
+                {
+                    ToJson(block.Docs, "onlyCaptures", isFirst);
+                    isFirst = false;
+
+                    // First scene shouldn't reqult in a previous scene being copied
+                    if (currentSceneIndex == -1)
+                    {
+                        currentSceneIndex = block.Index;
+                        currentSceneName = block.Name;
+
+                        ToCsv(block, "sceneName");
+                    }
+
+                    // If at a new scene > 0, then copy the previous
+                    else if (block.Index != currentSceneIndex)
+                    {
+                        CopyCsv(
+                            new SceneBlock()
+                            {
+                                Name = currentSceneName,
+                                Index = currentSceneIndex,
+                            }, "sceneName");
+
+                        currentSceneIndex = block.Index;
+                        currentSceneName = block.Name;
+
+                        ToCsv(block, "sceneName");
+                    }
+                    else
+                    {
+                        ToCsv(block, "sceneName");
+                    }
+                }
+
+                // Copy the last block
+                if (i == workloads.Count - 1)
+                {
+                    CopyCsv(sceneBlocks[sceneBlocks.Count - 1], "sceneName");
+                }
+            }
+
+            ToJsonEnd("raw");
+            ToJsonEnd("sessionInfo");
+            ToJsonEnd("onlyCaptures");
+
+            ToJsonStart("database");
+            ToJson(await GetSessionInfo(), "database");
+            ToJsonEnd("database");
+
+            CreateReadme();
+        }
+
+        /*
+         * Breaks the session collection into blocks, of a constant size or less
+         * Returns: Task<List<long>> of a broken down count of all the documents in a session
+         */
+        private static async Task<List<long>> GetWorkloads()
+        {
+            var sessionCollection = DB.GetCollection<BsonDocument>($"sessions.{sessionId}");
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            long docCount = await sessionCollection.CountDocumentsAsync(filter);
+
+            long groupCount = 1800; // 20 seconds worth of frames from a game running @90fps
+            int workloadCount = (int)(docCount / groupCount);
+            var workloads = new List<long>();
+
+            for (int i = 0; i < workloadCount; i++)
+            {
+                workloads.Add(groupCount);
+            }
+
+            // Add leftover, if it exists
+            // Leftover is always less than groupCount to prevent high memory usage
+            if (docCount % groupCount != 0)
+            {
+                workloads.Add(docCount - (groupCount * workloadCount));
+            }
+
+            return workloads;
+        }
+
+        private static async Task<List<BsonDocument>> SortSession(int workload, int offset)
+        {
+            var sessionCollection = DB.GetCollection<BsonDocument>($"sessions.{sessionId}");
+
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var sorter = Builders<BsonDocument>.Sort
+                .Ascending(d => d["frameInfo"]["realtimeSinceStartup"]);
+            var projection = Builders<BsonDocument>.Projection
+                .Exclude("_id");
+
+            return await sessionCollection
+                .Find(filter)
+                .Skip(offset)
+                .Limit(workload)
+                .Sort(sorter)
+                .Project(projection)
+                .ToListAsync();
+        }
+
+        private static async Task<BsonDocument> GetSessionInfo()
+        {
+            var sessions = DB.GetCollection<BsonDocument>("sessions");
+
+            var filter = Builders<BsonDocument>.Filter
+                .Eq("id", sessionId);
+
+            return await sessions
+                .Find(filter)
+                .FirstOrDefaultAsync();
+        }
+
+        private static void ToJson(List<BsonDocument> sessionDocs, string about, bool isFirst, JsonWriterSettings ws = null)
+        {
+            StringBuilder docsTotal = new StringBuilder();
+
+            foreach (BsonDocument d in sessionDocs)
+            {
+                docsTotal.AppendFormat("{0}{1}", ",", d.ToJson(ws ?? JsonWriterSettings.Defaults));
+            }
+
+            // Remove leading comma
+            if (isFirst)
+            {
+                docsTotal.Remove(0, 1);
+            }
+
+            string filename = $"{sessionId}.{about}.json";
+            AppendToFile(docsTotal.ToString(), filename);
+        }
+
+        private static void ToJsonStart(string about)
+        {
+            AppendToFile("[", $"{sessionId}.{about}.json");
+        }
+
+        private static void ToJsonEnd(string about)
+        {
+            AppendToFile("]", $"{sessionId}.{about}.json");
+        }
+
+        private static void ToJson(BsonDocument sessionDoc, string about)
+        {
+            string filename = $"{sessionId}.{about}.json";
+            AppendToFile(sessionDoc.ToJson(JsonWriterSettings.Defaults), filename);
+        }
+
+        private static string ToFlatJson(List<BsonDocument> sessionDocs)
+        {
+            StringBuilder docsTotal = new StringBuilder();
+            docsTotal.Append("[");
+
+            foreach (BsonDocument d in sessionDocs)
+            {
+                string json = d.ToJson(JsonWriterSettings.Defaults);
+                var dict = JsonHelper.DeserializeAndFlatten(json);
+                string flat = Newtonsoft.Json.JsonConvert.SerializeObject(dict);
+
+                docsTotal.AppendFormat("{0}{1}", flat, ",");
+            }
+
+            docsTotal.Length--; // Remove trailing comma
+            docsTotal.Append("]");
+
+            return docsTotal.ToString();
+        }
+
+        private static void ConfigureJsonWriter()
+        {
+            JsonWriterSettings.Defaults = new JsonWriterSettings()
+            {
+                Indent = true,
+                OutputMode = JsonOutputMode.Strict,
+            };
+        }
+
+        private static Configuration GetConfiguration()
+        {
+            return new Configuration
+            {
+                ShouldSkipRecord = (record) => record.All(string.IsNullOrEmpty),
+                MissingFieldFound = (record, index, context) => record.All(string.IsNullOrEmpty),
+                Delimiter = ",",
+            };
+        }
+
+        private static (List<BsonDocument> otherCaptures, List<SceneBlock> scenes) ProcessScenes(List<BsonDocument> sessionDocs)
+        {
+            var sceneDocs = sessionDocs.FindAll(d => d.Contains("sceneName"));
+            int sceneIndex = currentSceneIndex;
+
+            List<SceneBlock> sceneMap = sceneDocs.Select((scene) =>
+            {
+                sceneIndex++;
+                return new SceneBlock()
+                {
+                    StartTime = scene["frameInfo"]["realtimeSinceStartup"].AsDouble,
+                    Name = scene["sceneName"].AsString,
+                    Index = sceneIndex,
+                    Docs = new List<BsonDocument>(),
+                };
+            }).ToList();
+
+            var otherCaptures = sessionDocs.FindAll(d => !d.Contains("gameObjects"));
+            var normalCaptures = sessionDocs.FindAll(d => d.Contains("gameObjects"));
+
+            // If there are no scene captures, return a default block
+            if (sceneMap.Count == 0)
+            {
+                return (
+                    otherCaptures,
+                    new List<SceneBlock>()
+                    {
+                        new SceneBlock()
+                        {
+                            Name = currentSceneName,
+                            Index = sceneIndex,
+                            Docs = normalCaptures,
+                        },
+                    });
+            }
+
+            // Otherwise, process each block by scene
+            int index = 0;
+            var currentScene = sceneMap[index];
+
+            for (int i = 0; i < normalCaptures.Count; i++)
+            {
+                var currentDoc = normalCaptures[i];
+                var currentTimestamp = currentDoc["frameInfo"]["realtimeSinceStartup"].AsDouble;
+                int tempIndex = sceneIndex + 1;
+
+                if (tempIndex < sceneMap.Count
+                    && currentTimestamp > sceneMap[tempIndex].StartTime)
+                {
+                    index++;
+                    currentScene = sceneMap[index];
+                }
+
+                currentScene.Docs.Add(currentDoc);
+            }
+
+            return (otherCaptures, sceneMap);
+        }
+
+        private static void ToCsv(SceneBlock block, string about)
+        {
+            string json = ToFlatJson(block.Docs);
+            string csv = JsonToCsv(json);
+
+            string path = $"temporary{Seperator}CSVs{Seperator}";
+            AppendToFile(csv, $"{sessionId}.{about}.{block.Name}.{block.Index}.csv", path);
+        }
+
+        private static string JsonToCsv(string jsonContent)
+        {
+            StringWriter csvString = new StringWriter();
+
+            using (var csv = new CsvWriter(csvString, GetConfiguration()))
+            {
+                var dt = JsonHelper.JsonToTable(jsonContent);
+                dt = AlignHeaders(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    for (var i = 0; i < dt.Columns.Count; i++)
+                    {
+                        csv.WriteField(row[i]);
+                    }
+
+                    csv.NextRecord();
+                }
+            }
+
+            return csvString.ToString();
+        }
+
+        private static void CopyCsv(SceneBlock block, string about)
+        {
+            StringWriter csvString = new StringWriter();
+
+            using (var csv = new CsvWriter(csvString, GetConfiguration()))
+            {
+                foreach (string key in currentHeader)
+                {
+                    csv.WriteField(key);
+                }
+
+                csv.NextRecord();
+            }
+
+            string path = $"exported{Seperator}CSVs{Seperator}";
+            string filename = $"{sessionId}.{about}.{block.Name}.{block.Index}.csv";
+            AppendToFile(csvString.ToString(), filename, path);
+
+            string temporaryLocation = $"{prefix}temporary{Seperator}CSVs{Seperator}{filename}";
+            File.AppendAllText($"{prefix}{path}{filename}", File.ReadAllText(temporaryLocation));
+
+            // Reset the headers, because a new scene will likely have differerent headers
+            currentHeader = new List<string>();
+        }
+
+        // TODO: make less magic
+        // TODO: check this works for any data
+        private static DataTable AlignHeaders(DataTable dt)
+        {
+            var header = dt.Columns.Cast<DataColumn>()
+                        .Select(col => col.ColumnName)
+                        .ToList();
+
+            if (!header.SequenceEqual(currentHeader))
+            {
+                // Store all new headers in the global header var
+                foreach (string h in header)
+                {
+                    if (!currentHeader.Contains(h))
+                    {
+                        currentHeader.Add(h);
+                    }
+                }
+
+                // Add all headers the current block lacks into it
+                foreach (string h in currentHeader)
+                {
+                    if (!header.Contains(h))
+                    {
+                        dt.Columns.Add(h);
+                    }
+                }
+            }
+
+            // Order headers for the current block
+            for (int i = 0; i < currentHeader.Count; i++)
+            {
+                dt.Columns[currentHeader[i]].SetOrdinal(i);
+            }
+
+            return dt;
+        }
+
+        private static void CreateReadme()
+        {
+            string source = System.IO.File.ReadAllText($".{Seperator}Templates{Seperator}README.txt.handlebars");
+            var template = Handlebars.Compile(source);
+
+            var data = new
+            {
+                id = sessionId,
+            };
+
+            AppendToFile(template(data), "README.txt");
+        }
+
+        private static void AppendToFile(string content, string filename, string path = "")
+        {
+            string fullpath;
+            if (path == string.Empty)
+            {
+                fullpath = $"{prefix}exported{Seperator}{filename}";
+            }
+            else
+            {
+                fullpath = $"{prefix}{path}{filename}";
+            }
+
+            using (var sw = File.AppendText(fullpath))
+            {
+                sw.Write(content);
+            }
+        }
+
+        private static void ZipFolder(string location, string outName)
+        {
+            ZipFile.CreateFromDirectory(location, outName);
+        }
+
+        // From: https://docs.min.io/docs/dotnet-client-quickstart-guide.html
+        private static async Task Upload(string fileLocation)
+        {
+            var bucketName = "sessions.exported";
+
+            // Regions shouldn't really matter because the zip is uploaded to local minio anyway
+            var location = "us-west-1";
+            var objectName = $"{sessionId}_session_exported.zip";
+            var filePath = fileLocation;
+            var contentType = "application/zip";
+
+            try
+            {
+                // Make a bucket on the server, if not already present.
+                bool found = await OS.BucketExistsAsync(bucketName);
+                if (!found)
+                {
+                    await OS.MakeBucketAsync(bucketName, location);
+                }
+
+                // Upload a file to bucket.
+                await OS.PutObjectAsync(bucketName, objectName, filePath, contentType);
+            }
+            catch (MinioException e)
+            {
+                Console.WriteLine("File Upload Error: {0}", e.Message);
+            }
+        }
+
+        private static async Task UpdateDoc()
+        {
+            var sessions = DB.GetCollection<BsonDocument>("sessions");
+
+            var filter = Builders<BsonDocument>.Filter
+                .Eq("id", sessionId);
+
+            var update = Builders<BsonDocument>.Update
+                .Set("isExported", true)
+                .Set("isPending", false);
+
+            await sessions.UpdateOneAsync(filter, update);
+        }
+    }
+
+    // Used to group together captures based on timestamps
+    internal class SceneBlock
+    {
+        #pragma warning disable SA1516
+        public string Name { get; set; }
+        public double StartTime { get; set; }
+        public int Index { get; set; }
+        public List<BsonDocument> Docs { get; set; }
+        #pragma warning restore SA151
+    }
+}
