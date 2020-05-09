4e9db10a5f3c7df0d2eb387b9f5aa624a544a092
jhburns <jburns@chapman.edu>
2020-05-09 00:18:50 -0700

    Fixing CI

--- SetupTestGame/build.sh
+++ SetupTestGame/build.sh
@@ -5,20 +5,10 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     /opt/Unity/Editor/Unity \
     -quit  \
     -batchmode \
-    -buildTarget Win64 \
     -silent-crashes \
     -username "$username" \
     -password "$password" \
-    -serial "$serial" \
-    /app
-
-status1=$?
-
-if [ "$status1" -ne 0 ];
-then
-    echo "Error: Could not aquire license"
-    exit "$status1"
-fi
+    -serial "$serial"
 
 # Run a build with outut
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
