24f3a04abf4a5a4736f2182914ab3fe438cd78d9
jhburns <jburns@chapman.edu>
2020-05-09 11:32:02 -0700

    Fixing CI

--- ClientDevelopmentGame/build.sh
+++ ClientDevelopmentGame/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+set -e
+USERNAME=username
+PASSWORD=password
+SERIAL=serial
+
 # Get the license and build first because we have too
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     /opt/Unity/Editor/Unity \
     -quit  \
     -batchmode \
     -silent-crashes \
-    -username "$username" \
-    -password "$password" \
-    -serial "$serial"
+    -username "$USERNAME" \
+    -password "$PASSWORD" \
+    -serial "$SERIAL"
 
 # Run a build with outut
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
