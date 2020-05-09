883682f6f8946a3fe578185361bf55d458b2c55d
jhburns <jburns@chapman.edu>
2020-05-08 23:39:37 -0700

    Fixing build.sh

--- SetupTestGame/build.sh
+++ SetupTestGame/build.sh
@@ -5,12 +5,20 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     /opt/Unity/Editor/Unity \
     -quit  \
     -batchmode \
-    -silent-crashes \
     -buildTarget Win64 \
+    -silent-crashes
     -username "$username" \
     -password "$password" \
     -serial "$serial" \
     /app
+
+status1=$?
+
+if [ "$status1" -ne 0 ];
+then
+    echo "Error: Could not aquire license"
+    exit "$status1"
+fi
 
 # Run a build with outut
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
@@ -22,10 +30,10 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     -buildTarget Win64 \
     /app
 
-status=$?
+status2=$?
 
 # Return license
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     /opt/Unity/Editor/Unity -quit -batchmode -returnlicense -silent-crashes
 
-exit "$status"
+exit "$status2"
