d290d83bd21e3cd239efa33fc66d34194914c9d4
Jhburns <jburns@chapman.edu>
2020-05-31 03:16:03 -0700

    Adding warning text to login button

--- ClientDevelopmentGame/Assets/ExperienceCapture/Subassets/CaptureSetup.cs
+++ ClientDevelopmentGame/Assets/ExperienceCapture/Subassets/CaptureSetup.cs
@@ -20,7 +20,7 @@ public class CaptureSetup : MonoBehaviour
 
     [Tooltip("Label the game version before releasing.")]
     public string gameVersion;
-    public const string clientVersionLocked = "1.2.0";
+    public const string clientVersionLocked = "1.2.1";
     [Tooltip("Don't edit, is readonly and only informational.")]
     public string clientVersion;
 
@@ -50,6 +50,7 @@ public class CaptureSetup : MonoBehaviour
     public InputField urlInput;
     public Text openingInfo;
     public Text connectionInfo;
+    public Text warningInfo;
 
     public Text sessionInfo;
     private string sessionInfoSave;
@@ -84,6 +85,7 @@ public class CaptureSetup : MonoBehaviour
             newSession.gameObject.SetActive(false);
             urlTitle.gameObject.SetActive(false);
             urlInput.gameObject.SetActive(false);
+            warningInfo.gameObject.SetActive(false);
         }
         else
         {
@@ -115,6 +117,8 @@ public class CaptureSetup : MonoBehaviour
     {
         urlTitle.gameObject.SetActive(false);
         urlInput.gameObject.SetActive(false);
+        warningInfo.gameObject.SetActive(false);
+
         newSession.gameObject.SetActive(false);
 
         connectionInfo.gameObject.SetActive(true);
@@ -153,6 +157,7 @@ public class CaptureSetup : MonoBehaviour
 
                 urlTitle.gameObject.SetActive(true);
                 urlInput.gameObject.SetActive(true);
+                warningInfo.gameObject.SetActive(true);
 
                 newSession.gameObject.SetActive(true);
             })
