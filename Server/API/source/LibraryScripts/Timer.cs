7f0ede692711b7756ebffa6c79d4f23566849ac3
jhburns <jburns@chapman.edu>
2020-06-01 15:22:25 -0700

    Adding expiration time to tokens

--- Server/API/source/LibraryScripts/Timer.cs
+++ Server/API/source/LibraryScripts/Timer.cs
@@ -3,12 +3,17 @@ namespace Carter.App.Lib.Timer
     using System;
     using MongoDB.Bson;
 
-    public static class CheckExpire
+    public static class TimerExtra
     {
         public static bool IsAfter(this BsonDateTime start, int expirationTime)
         {
             BsonDateTime endTime = new BsonDateTime(DateTime.Now.AddSeconds(-expirationTime));
             return start.CompareTo(endTime) < 0;
+        }
+
+        public static BsonDateTime ProjectSeconds(int expirationTime)
+        {
+            return new BsonDateTime(DateTime.Now.AddSeconds(expirationTime));
         }
     }
 }
