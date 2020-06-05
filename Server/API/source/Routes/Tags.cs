db3f2c1022ed977b467a9c87d763e5895669a042
jhburns <jburns@chapman.edu>
2020-06-04 20:45:47 -0700

    Spellchecking

--- Server/API/source/Routes/Tags.cs
+++ Server/API/source/Routes/Tags.cs
@@ -17,11 +17,11 @@ namespace Carter.App.Route.Tags
     public class Tags : CarterModule
     {
         public Tags(
-            IRepository<AccessTokenSchema> accesRepo,
+            IRepository<AccessTokenSchema> accessRepo,
             IMongoDatabase db)
             : base("/sessions/{id}/tags")
         {
-            this.Before += PreSecurity.GetSecurityCheck(accesRepo);
+            this.Before += PreSecurity.GetSecurityCheck(accessRepo);
 
             this.Post("/{tagName}", async (req, res) =>
             {
