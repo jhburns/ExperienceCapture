90a9ce73fa5e83da2256e149720e7774c2bc8e12
jhburns <jburns@chapman.edu>
2020-06-10 16:26:38 -0700

    Adding tests for about component

---
+++ Server/WebUI/src/__tests__/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow, mount, } from 'enzyme';
+import About from 'components/About';
+
+it('has non-empty content', () => {
+  const message = shallow(<About />);
+
+  expect(message.text().length).toBeGreaterThan(0);
+});
+
+it('exists', () => {
+  const message = mount(<About />);
+
+  expect(message.find('a').exists()).toBeTruthy();
+});
+
+it('Initial state is closed', () => {
+  const message = mount(<About />);
+
+  expect(message.state().isOpen).toBe(false);
+});
