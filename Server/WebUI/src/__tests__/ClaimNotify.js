f4bbf3f5576afb0677f879998175f676d3ac9008
jhburns <jburns@chapman.edu>
2020-06-10 16:53:32 -0700

    Linting

--- Server/WebUI/src/__tests__/ClaimNotify.js
+++ Server/WebUI/src/__tests__/ClaimNotify.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { shallow, mount, } from 'enzyme';
 import ClaimNotify from 'components/ClaimNotify';
 
-import { StaticRouter as Router, StaticRouter } from "react-router-dom";
+import { StaticRouter as Router } from "react-router-dom";
 
 it('has non-empty content', () => {
-  const message = shallow(<StaticRouter><ClaimNotify /></StaticRouter>);
+  const message = shallow(<Router><ClaimNotify /></Router>);
 
   expect(message.text().length).toBeGreaterThan(0);
 });
 
 it('exists', () => {
-  const message = mount(<StaticRouter><ClaimNotify /></StaticRouter>);
+  const message = mount(<Router><ClaimNotify /></Router>);
 
   expect(message.find('span').exists()).toBeTruthy();
   expect(message.text().includes('✔️')).toBeTruthy();
