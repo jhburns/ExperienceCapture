81cbcc747a708a21f03bb2ba6d5006e93f359026
jhburns <jburns@chapman.edu>
2020-06-10 19:45:15 -0700

    Adding session table tests

--- Server/WebUI/src/__tests__/SessionTable.js
+++ Server/WebUI/src/__tests__/SessionTable.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { shallow, mount, } from 'enzyme';
 import SessionTable from 'components/SessionTable';
 
-import { StaticRouter as Router } from "react-router-dom";
-import validator from 'validator';
-
 it('has non-empty content', () => {
   const row = shallow(<SessionTable />);
 
   expect(row.text().length).toBeGreaterThan(0);
+});
+
+it('lacks button header when undefined', () => {
+  const row = mount(<SessionTable />);
+
+  expect(row.find('th')).toHaveLength(3);
+});
+
+it('has button header when defined', () => {
+  const row = mount(<SessionTable buttonData={{}} />);
+
+  expect(row.find('th')).toHaveLength(4);
+});
+
+it('uses empty text when items is empty', () => {
+  const row = mount(<SessionTable sessions={[]} emptyMessage="example" />);
+
+  expect(row.text().includes("example")).toBeTruthy();
 });
