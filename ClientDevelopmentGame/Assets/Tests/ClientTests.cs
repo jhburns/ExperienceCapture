7b8efd91697d2a46aa468badac4b3c94d188943f
Jhburns <jburns@chapman.edu>
2020-06-11 23:47:27 -0700

    Getting assemblies to work cross client

--- ClientDevelopmentGame/Assets/Tests/ClientTests.cs
+++ ClientDevelopmentGame/Assets/Tests/ClientTests.cs
@@ -3,17 +3,23 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class ClientTests {
+using Capture.Internal.Client;
+using Capture.Internal.InputStructure;
+
+public class ClientTests
+{
 
     [Test]
-    public void ClientTestsSimplePasses() {
+    public void ClientTestsSimplePasses()
+    {
         // Use the Assert class to test conditions.
     }
 
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
-    public IEnumerator ClientTestsWithEnumeratorPasses() {
+    public IEnumerator ClientTestsWithEnumeratorPasses()
+    {
         // Use the Assert class to test conditions.
         // yield to skip a frame
         yield return null;
