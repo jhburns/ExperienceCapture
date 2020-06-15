f293c38692af68d0f34293f5fac35a222e32ef50
jhburns <jburns@chapman.edu>
2020-06-15 14:26:10 -0700

    Adding cross origin from developing so cypress can give better errors

--- Server/Cypress/cypress/integration/example.js
+++ Server/Cypress/cypress/integration/example.js
@@ -1,5 +1,11 @@
 describe('My First Test', () => {
+  beforeEach(() => {
+    // Sign In
+    cy.visit("/");
+    cy.get('[data-cy=go-home]').click();
+  })
+
   it('Does not do much!', () => {
-    expect(true).to.equal(true)
+    expect(true).to.equal(true);
   })
 })
