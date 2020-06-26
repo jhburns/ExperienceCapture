b803e0dc46f2ae8ce290d6ba6aafd382d5fbe150
jhburns <jburns@chapman.edu>
2020-06-25 20:33:00 -0700

    Refactoring fetch extra

--- Server/WebUI/src/libs/fetchExtra.js
+++ Server/WebUI/src/libs/fetchExtra.js
@@ -1,48 +1,41 @@
 // TODO: refactor this to use less repetition
 // TODO: check or recover from access token expiration
-async function postData(url = '', data = {}) {
-  const response = await fetch(url, {
-    method: 'POST',
-    mode: 'cors', 
-    cache: 'no-cache', 
+function config(customizations = {}) {
+  const base = {
+    mode: 'cors',
+    cache: 'no-cache',
     credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json'
     },
     redirect: 'follow',
     referrerPolicy: 'no-referrer',
+  };
+
+  const merged = Object.assign(base, customizations) 
+
+  return merged
+}
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, config({
+    method: 'POST',
     body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
+  }));
   return await response;
 }
 
 async function getData(url = '') {
-  const response = await fetch(url, {
+  const response = await fetch(url, config({
     method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-  });
+  }));
   return await response;
 }
 
 async function deleteData(url = '') {
-  const response = await fetch(url, {
+  const response = await fetch(url, config({
     method: 'DELETE',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-  });
+  }));
   return await response;
 }
 
