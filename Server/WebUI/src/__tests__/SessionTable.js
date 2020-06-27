f57fa7928d69d78382763b959cbf0dfc32cdfc2b
jhburns <jburns@chapman.edu>
2020-06-26 17:55:49 -0700

    Fixing Session Table tests

--- Server/WebUI/src/__tests__/SessionTable.js
+++ Server/WebUI/src/__tests__/SessionTable.js
@@ -3,25 +3,25 @@ import { shallow, mount, } from 'enzyme';
 import SessionTable from 'components/SessionTable';
 
 it('has non-empty content', () => {
-  const table = shallow(<SessionTable />);
+  const table = shallow(<SessionTable queryOptions={{}} />);
 
   expect(table.text().length).toBeGreaterThan(0);
 });
 
 it('lacks button header when undefined', () => {
-  const table = mount(<SessionTable />);
+  const table = mount(<SessionTable queryOptions={{}} />);
 
   expect(table.find('th')).toHaveLength(3);
 });
 
 it('has button header when defined', () => {
-  const table = mount(<SessionTable buttonData={{}} />);
+  const table = mount(<SessionTable queryOptions={{}} buttonData={{}} />);
 
   expect(table.find('th')).toHaveLength(4);
 });
 
 it('uses empty text when items is empty', () => {
-  const table = mount(<SessionTable sessions={[]} emptyMessage="example" />);
+  const table = mount(<SessionTable queryOptions={{}} sessions={[]} emptyMessage="example" />);
 
   expect(table.text().includes("example")).toBeTruthy();
 });
