858ac63253f1e5edca55b7b6cdb1d449da7660c8
jhburns <jburns@chapman.edu>
2020-07-03 23:02:14 -0700

    Fixing claim cypress test

--- Server/Cypress/cypress/integration/claim.js
+++ Server/Cypress/cypress/integration/claim.js
@@ -2,7 +2,8 @@ describe('Claim Page', () => {
   it('Can get a claim and redeem it.', () => {
     cy.request({ method: 'POST', url: '/api/v1/authentication/claims', failOnStatusCode: true })
       .then((response) => {
-        cy.visit(`/signInFor?claimToken=${response.body.claimToken}`)
+        const encodedClaim = encodeURIComponent(response.body.claimToken);
+        cy.visit(`/signInFor?claimToken=${encodedClaim}`)
           .then(() => {
             cy.get('[data-cy=go-root]')
               .click();
