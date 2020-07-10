6cb26731e13393a317cc49384a544a549db29573
jhburns <jburns@chapman.edu>
2020-07-09 20:54:07 -0700

    Adding first route metadata

---
+++ ECBackend/API/source/MetaData/Sessions.cs
@@ -0,0 +1,12 @@
+namespace Carter.App.MetaData.Sessions
+{
+    using System.Collections.Generic;
+
+    using Carter.App.Route.Sessions;
+
+    using Carter.OpenApi;
+
+    public class GetSessionsMetaData : RouteMetaData
+    {
+    }
+}
