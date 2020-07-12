b74037676f13880818dcfae066fbbc1d2cf91cbd
jhburns <jburns@chapman.edu>
2020-07-11 21:18:01 -0700

    Adding an updated data sample for examples

--- ExampleAnalyzers/PythonWithJSON/main.py
+++ ExampleAnalyzers/PythonWithJSON/main.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-about_filename = "./data/QN4F.sessionInfo.json"
-captures_filename = "./data/QN4F.onlyCaptures.json"
+about_filename = "./data/EGN4.sessionInfo.json"
+captures_filename = "./data/EGN4.onlyCaptures.json"
 
 with open(about_filename) as f:
     data = json.load(f)
