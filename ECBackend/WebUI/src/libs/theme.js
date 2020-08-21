d2fb3200c73290b950d08d5fdc2760c958f02369
jhburns <jburns@chapman.edu>
2020-08-20 18:00:23 -0700

    Adding font to site

--- ECBackend/WebUI/src/libs/theme.js
+++ ECBackend/WebUI/src/libs/theme.js
@@ -1,12 +1,25 @@
 import { makeTheme } from 'bootstrap-styled/lib/theme';
 
 const theme = makeTheme({
-  '$font-family-base': 'sans-serif',
-  '$body-bg': '#FFFFFF',
-  '$body-color': '#000000',
-  //'$btn-primary-bg': '#000000',
-  //'$btn-border-radius': '.035rem',
-  //'$link-color': '#000000',
+  // Colors
+  '$primary': '#6E2EEE',
+  '$warning': '#D50000',
+
+  // Base
+  '$font-family-base': 'Rubik, sans-serif',
+  'body-bg': '#F3F0F5',
+  'body-color': '#161617',
+
+  // Buttons
+  '$btn-primary-bg': '#6E2EEE',
+  '$btn-primary-color': '#F3F0F5',
+  '$btn-border-radius': '0.20rem',
+  '$btn-danger-bg': '#D50000',
+  '$btn-danger-color': '#FFF',
+
+  // Links
+  '$link-color': '#6E2EEE',
+  '$link-decoration': 'underline',
 });
 
 export default theme;
