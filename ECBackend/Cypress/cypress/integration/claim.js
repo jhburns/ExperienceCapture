518939a67694afa0ab9532e3a30c7e5fbc16762e
jhburns <jburns@chapman.edu>
2020-08-26 17:18:20 -0700

    Adding the claim page cypress test in again

--- ECBackend/Cypress/cypress/integration/claim.js
+++ ECBackend/Cypress/cypress/integration/claim.js
@@ -1,12 +1,12 @@
-/* Removing test for now, because this page is getting reworked anyway,
- * And this test will become outdated
 describe('Claim Page', () => {
   it('Can get a claim and redeem it.', () => {
     cy.request({ method: 'POST', url: '/api/v1/authentication/claims', failOnStatusCode: true })
       .then((response) => {
         const encodedClaim = encodeURIComponent(response.body.claimToken);
         cy.visit(`/?claimToken=${encodedClaim}`);
-      })
+      });
+
+    /* TODO: re-enable this or some type of navigation check
       .then(() => {
         cy.get('[data-cy=go-root]')
           .click();
@@ -14,6 +14,6 @@ describe('Claim Page', () => {
       .then(() => {
         cy.get('[data-cy=go-home]');
       });
+    */
   });
 });
-*/
